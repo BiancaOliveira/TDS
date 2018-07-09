7ea881486db35f82047c7e97a1829cad1cb2b223
marim.gabi <gabimarim06@gmail.com>
2018-07-09 17:45:10 -0300

    pau no meu cu

---
+++ src/biblioteca/control/telaInicialController.java
@@ -0,0 +1,32 @@
+package biblioteca.control;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller class
+ *
+ * @author gabriela
+ */
+
+public class telaInicialController implements Initializable {
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+
+
+    }
+
+
+
+
+}
